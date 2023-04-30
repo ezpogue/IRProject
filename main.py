ccb5431a851fe1ae4b57236d6217b8846e318cfc
ezpogue <ezpogue@gmail.com>
2023-04-29 18:49:05 -0700

    Added praw.ini, test code in main

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+import praw
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+reddit = praw.Reddit("IRProject")
+reddit.read_only = True
+print(reddit.read_only)
+for submission in reddit.subreddit("test").hot(limit=10):
+    print(submission.title)
